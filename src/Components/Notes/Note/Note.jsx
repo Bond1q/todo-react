405fb5e99b34334a0fa10ba090b25699fe769fd9
Bondi <example@gmail.com>
2021-05-12 00:00:53 +0300

    css changes

--- src/Components/Notes/Note/Note.jsx
+++ src/Components/Notes/Note/Note.jsx
@@ -6,8 +6,11 @@ const Note = ({ noteText, id, deleteNote, toggleCompletedNote, isNoteCompleted }
 	return (
 		<div className={styles.note}>
 			<div onDoubleClick={() => toggleCompletedNote(id)} className={isNoteCompleted ? styles.completed : undefined + " " + styles.noteEffect}>
-				{noteText}  <span onClick={() => deleteNote(id)}>×</span>
+				<button onClick={() => toggleCompletedNote(id)} className={isNoteCompleted ? styles.toComplete : styles.noDisplay}>✔️</button>
+				<button onClick={() => toggleCompletedNote(id)} className={isNoteCompleted ? styles.noDisplay : styles.inviz}> </button> {noteText}
+				<span className={styles.toDelete} onClick={() => deleteNote(id)}>×</span>
 			</div>
+
 		</div>
 	)
 }
